3c3ed1fecfa72565c13533f703ba694710a3c496
ilknur2603 <ilknurdayanc2603@gmail.com>
2023-03-09 18:14:28 -0700

    added files

---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+        //alert(response.message)
+        console.log(resposnse.message)
+      } else {
+        alert('Failed to sign up.');
+        alert(response.message);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
