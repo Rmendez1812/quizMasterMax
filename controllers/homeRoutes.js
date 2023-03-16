6f8eaf75df5613d65c3ace807e37b4ec6b22b62b
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-03-15 20:21:12 -0600

    debugged handlebars files and got css working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,44 +1,26 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get("/", async (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/login', (req, res) => {
- 
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
-router.get('/signup', (req, res) => {
- 
+router.get("/signup", (req, res) => {
   if (req.session.signup) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -30,7 +30,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      const response = await fetch('/api/users/signup', {
+      const response = await fetch('/api/users/sign-up', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
